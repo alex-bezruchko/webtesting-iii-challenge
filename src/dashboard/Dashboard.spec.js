fce0ed86f4f50c041694281feb9753c338098f30
Alex Bezruchko <alex.bezruchko@gmail.com>
2019-04-16 10:37:26 -0400

    add Dashboart Snapshot test

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,1 +1,12 @@
 // Test away
+import React from 'react';
+import renderer from 'react-test-renderer';
+import Dashboard from './Dashboard';
+
+describe('<Dashboard/>', () => {
+    it('matches snapshot', () => {
+        const domTree = renderer.create(<Dashboard />);
+
+        expect(domTree.toJSON()).toMatchSnapshot();
+    })
+})
