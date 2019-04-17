63e8c3f75ce448b637734b887ff4ab0b86a36f6f
Alex Bezruchko <alex.bezruchko@gmail.com>
2019-04-16 22:35:04 -0400

    unlocked shows on default

--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -3,11 +3,23 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
 import Display from './Display';
+import { render, fireEvent, cleanup } from 'react-testing-library';
+import "jest-dom/extend-expect";
+afterEach(cleanup);
 
 describe('<Display/>', () => {
     xit('matches snapshot', () => {
         const domTree = renderer.create(<Display />);
 
         expect(domTree.toJSON()).toMatchSnapshot();
+    })
+    it('unlocked shows on default', () => {
+        
+        const { getByText } = render(<Display />);
+
+        const button = getByText('Unlocked');
+
+        expect(button.textContent).toEqual("Unlocked");;
+
     })
 })
