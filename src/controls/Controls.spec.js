c6c4aec3f25b8d49e27663fe8320f61b513b87d5
Alex Bezruchko <alex.bezruchko@gmail.com>
2019-04-16 21:51:26 -0400

    test lock gate disabled & lock gate functioning

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -4,6 +4,7 @@ import React from 'react';
 import renderer from 'react-test-renderer';
 import { render, fireEvent, cleanup } from 'react-testing-library';
 afterEach(cleanup);
+import "jest-dom/extend-expect";
 
 import Controls from './Controls';
 
@@ -14,7 +15,7 @@ describe('<Controls />', () => {
         expect(domTree.toJSON()).toMatchSnapshot();
     })
 
-    it('is the unlock gate function working', () => {
+    it('is the close gate function working', () => {
         const mock = jest.fn();
         
         const { getByText } = render(<Controls toggleClosed={mock}/>);
@@ -23,5 +24,24 @@ describe('<Controls />', () => {
 
         fireEvent.click(button);
         expect(mock).toHaveBeenCalled();
+    })
+    it("by default lock gate is disabled", () => {
+        const { getByText } = render(<Controls />);
+        const lockButton = getByText(/Lock Gate/i);
+        expect(lockButton).toBeDisabled();
+    });
+
+   it('is the lock gate function working', () => {
+        const mock = jest.fn();
+        
+        const { getByText, queryByText } = render(<Controls toggleClosed={mock}/>);
+
+        const button = getByText("Close Gate");
+
+        fireEvent.click(button);
+
+        const lockGateButton = queryByText("Lock Gate");
+        // const button = queryByText("Lock Gate");
+        expect(lockGateButton.textContent).toEqual("Lock Gate");
     })
 })
